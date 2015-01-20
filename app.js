bad36204a3ce261aec70fa932ae72033a7f21fe9
smturek <smturek@gmail.com>
2015-01-20 15:26:30 -0700

    added music

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 "use strict";
 
-var song1 = document.getElementsByTagName("audio")[0];
+var hours = document.getElementsByTagName("audio")[0];
+var friends = document.getElementsByTagName("audio")[1];
+var time = document.getElementsByTagName("audio")[2];
+var touka = document.getElementsByTagName("audio")[3];
+
+var pause = function() {
+  hours.pause();
+  friends.pause();
+  time.pause();
+  touka.pause();
+};
+
+$.getJSON('tracks.json', function(tracks) {
+  var $tracksTemplate = $('[data-template=tracks]').html();
+
+  var newHTML = Mustache.to_html($tracksTemplate, tracks);
+
+  $('.container').html(newHTML);
+});
 
 $('.container').on('click', 'i.fa', function() {
-  $('i.fa').removeClass('fa-pause').addClass('fa-play');
+  $('i.fa').removeClass('fa-stop').addClass('fa-play');
 
-  $(this).removeClass('fa-play').addClass('fa-pause');
+  $(this).removeClass('fa-play').addClass('fa-stop');
 });
 
-$('.container').on('click', 'i.fa-pause', function() {
-  $(this).removeClass('fa-pause').addClass('fa-play');
+$('.container').on('click', 'i.fa-stop', function() {
+  $(this).removeClass('fa-stop').addClass('fa-play');
 });
 
-$('.song').on("click", function() {
-  song1.load();
-  song1.play();
+$('.hours').on("click", 'i.fa-play', function() {
+  hours.load();
+  pause();
+  hours.play();
+});
+
+$('.friends').on("click", 'i.fa-play', function() {
+  friends.load();
+  pause();
+  friends.play();
+});
+
+$('.time').on("click", 'i.fa-play', function() {
+  time.load();
+  pause();
+  time.play();
+});
+
+$('.touka').on("click", 'i.fa-play', function() {
+  touka.load();
+  pause();
+  touka.play();
 });
